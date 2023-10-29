3d50a287a0ea60dbcb8c6bfd2f93ba0aa19bb0af
max <max.assis@chatguru.com.br>
2023-10-29 16:38:47 -0300

    feito os novos mocks

--- src/functions/requests.ts
+++ src/functions/requests.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 import { createZodFetcher } from "zod-fetch";
 
 const fetcher = createZodFetcher();
-const environment = "dev"  // dev | prod
+const environment = "dev"; // dev | prod
 
 // REQUISIÇÃO DE TAGS
 const schemaTags = z.array(
@@ -19,7 +19,10 @@ export type Tags = z.infer<typeof schemaTags>;
 
 export function fetchTags() {
   const dt = ref<Tags>([]);
-  const url = environment === "dev" ? "http://localhost:3000/tags" : "";
+  const url =
+    environment === "dev"
+      ? "https://run.mocky.io/v3/6b4cb26a-02fb-4792-8d53-f42492e7ca75"
+      : "";
 
   fetcher(schemaTags, url)
     .then((response) => (dt.value = response))
@@ -49,7 +52,10 @@ export const schemaDpt = z.object({
 export type Dpt = z.infer<typeof schemaDpt>;
 export function fetchDpt() {
   const dt = ref<Dpt | undefined>(undefined);
-  const url =  environment === "dev" ? "http://localhost:3000/users_and_groups" :  "";
+  const url =
+    environment === "dev"
+      ? "https://run.mocky.io/v3/e2053a35-39e2-4b02-b9c3-7908267d3612"
+      : "";
 
   fetcher(schemaDpt, url)
     .then((response) => {
@@ -78,7 +84,10 @@ export type Funis = z.infer<typeof schemaFunnels>;
 
 export function fetchFunnels() {
   const dt = ref<Funis | []>([]);
-  const url = environment === "dev" ? "http://localhost:3000/funnels" : "";
+  const url =
+    environment === "dev"
+      ? "https://run.mocky.io/v3/8586f883-b690-4365-8989-b6ee66dea738"
+      : "";
 
   fetcher(schemaFunnels, url)
     .then((response) => (dt.value = response))
@@ -90,17 +99,20 @@ export function fetchFunnels() {
 // REQUISIÇÂO DE DEVICES
 export const schemaDevices = z.array(
   z.object({
-     id: z.string().min(1),
-     description: z.string().min(1),
-     owner_wa_id: z.string() 
-    })
-  )
+    id: z.string().min(1),
+    description: z.string().min(1),
+    owner_wa_id: z.string(),
+  })
+);
 
-export type Devices = z.infer<typeof schemaDevices>; 
+export type Devices = z.infer<typeof schemaDevices>;
 
 export function fetchDevices() {
   const dt = ref<Devices>([]);
-  const url = environment === "dev" ? "http://localhost:3000/devices" : "";
+  const url =
+    environment === "dev"
+      ? "https://run.mocky.io/v3/9ec958b0-004f-4cea-a2b3-c8ba266d5064"
+      : "";
 
   fetcher(schemaDevices, url)
     .then((response) => (dt.value = response))
@@ -108,7 +120,6 @@ export function fetchDevices() {
 
   return dt;
 }
-
 
 // REQUISIÇÂO CHATS
 // export const schemaChats = z.object({
@@ -142,3 +153,38 @@ export function fetchDevices() {
 //   )
 // })
 
+// TESTE REQUEST
+
+// export const schemaTeste = z.object({
+//   id: z.number().min(1),
+//   title: z.string().min(1),
+//   body: z.string().min(1),
+//   userId: z.number().min(1),
+// });
+
+// export type Teste = z.infer<typeof schemaTeste>;
+
+// export function fetchTeste() {
+//   const dt = ref<Teste>();
+
+//   fetcher(schemaTeste, "https://jsonplaceholder.typicode.com/posts", {
+//     method: "POST",
+//     body: JSON.stringify({
+//       title: "foo",
+//       body: "bar",
+//       userId: 1,
+//     }),
+//     headers: {
+//       "Content-type": "application/json; charset=UTF-8",
+//     },
+//   })
+//     .then((response) => {
+//       dt.value = response;
+//       console.log(response);
+//     })
+//     .catch((error) => console.log(error));
+
+//   return dt;
+// }
+
+// fetchTeste()
